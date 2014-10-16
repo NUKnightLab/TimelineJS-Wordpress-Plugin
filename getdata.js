7e064e3d939cd97102a158a992e94c24054c10ca
Suyeon Son <suyeon.son@icloud.com>
2014-10-16 15:35:02 -0500

    update errthang

--- getdata.js
+++ getdata.js
@@ -1,6 +1,9 @@
+var params = {
+  key: "1kekecMN31fxge0j"
+};
 
-
-$.getJSON('http://api.asg.northwestern.edu/terms/', params, function(data) {
+$.getJSON('http://api.asg.northwestern.edu/courses/?key=YOUR_KEY&term=4560&subject=SPANISH', params, function(data) {
   // data is the JavaScript array that results from parsing the response
   console.log(data);
 });
+

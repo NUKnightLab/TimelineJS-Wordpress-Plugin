014b23e2a414428cccb1f0d92534dd9affd138f9
Suyeon Son <suyeon.son@icloud.com>
2014-10-17 14:08:06 -0500

    update w api key

--- getdata.py
+++ getdata.py
@@ -1,7 +1,7 @@
 import requests
 
 params = {
-  'key': YOUR_KEY
+  'key': '1kekecMN31fxge0j'
 }
 response = requests.get('http://api.asg.northwestern.edu/terms/', params=params)
 # calling response.json() returns the response as a Python dictionary or list

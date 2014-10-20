0aec4d9882be3ce8ef5acb7b6745cd317b63481e
Suyeon Son <suyeon.son@icloud.com>
2014-10-20 14:20:05 -0500

    make fb log in function

--- script.js
+++ script.js
@@ -8,12 +8,42 @@ window.fbAsyncInit = function() {
 	xfbml      : true,
 	version    : 'v2.1'
 	});
+
+	function fbLogIn() {
+		Parse.FacebookUtils.logIn(null, {
+			success: function(user) {
+				if (!user.existed()) {
+					alert("User signed up and logged in through Facebook!");
+				} else {
+					alert("User logged in through Facebook!");
+				}
+		},
+			error: function(user, error) {
+				alert("User cancelled the Facebook login or did not fully authorize.");
+			}
+		});
+	};
+
+
+	// FB.getLoginStatus(function(response) {
+	// 	if (response.status === "connected") {
+	// 		$("button").text("log out?")
+	// 		Parse.User.logOut();
+	// 	} else {
+	// 			$("button").click(function() {
+	// 				fbLogIn();
+	// 			});
+	// 	}
+	// })
+
 };
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1569324956622321&version=v2.0";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1569324956622321&version=v2.1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+

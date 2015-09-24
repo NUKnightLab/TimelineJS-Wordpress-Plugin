e6afff2589e8a06326886152575b687096d48c42
Joe Germuska <joe@germuska.com>
2015-09-24 16:32:24 -0500

    following rec from https://codex.wordpress.org/Writing_a_Plugin

--- knightlab-timeline.php
+++ knightlab-timeline.php
@@ -15,6 +15,7 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+defined( 'ABSPATH' ) or die( 'nope' );
 
 define( 'KL_TIMELINE_URL', plugin_dir_url(__FILE__) );
 
